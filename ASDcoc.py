ed2d08234fb59d28f7b4ef9ed8b08e7a4c94306d
Daniel San Martin <dsanmartins@gmail.com>
2018-05-22 22:09:29 -0300

    We rearrange the widgets and add new fields in database.

--- ASDcoc.py
+++ ASDcoc.py
@@ -21,7 +21,7 @@ conn = sqlite3.connect('smell.db')
 def createTables():
     c = conn.cursor()
     # Create table if exists
-    c.execute('''CREATE TABLE IF NOT EXISTS smell (Smell text, Description text, rationale text, causes text, example blob, affected text, abstractions text, considerations text)''')
+    c.execute('''CREATE TABLE IF NOT EXISTS smell (Smell text, Description text, rationale text, causes text, exampleDesc text, figure blob, pngpath text, affected text, abstractions text, considerations text)''')
     conn.commit()
 
 def insertSmell():
@@ -31,6 +31,7 @@ def insertSmell():
     description = app.getTextArea("Description")
     rationale = app.getTextArea("implRationale") 
     causes = app.getTextArea("implPotential")
+    exampleDesc = app.getTextArea("exDesc")
     example = app.getEntry("f1")
     affected = app.getTextArea("implQualityAttr")
     abstractions = app.getTextArea("implAbstrAffec")
@@ -42,15 +43,15 @@ def insertSmell():
         if example.endswith('png'):
             png = open(example,'rb')
             ablob = png.read()
-            smellDocumentation = [(smellName, description, rationale, causes, sqlite3.Binary(ablob), affected, abstractions, considerations)]
-            c.executemany('INSERT INTO smell VALUES (?,?,?,?,?,?,?,?)', smellDocumentation)
+            smellDocumentation = [(smellName, description, rationale, causes, exampleDesc, sqlite3.Binary(ablob), example, affected, abstractions, considerations)]
+            c.executemany('INSERT INTO smell VALUES (?,?,?,?,?,?,?,?,?,?)', smellDocumentation)
             conn.commit()
             app.clearAllEntries(callFunction=False)
             app.clearAllTextAreas(callFunction=False)
             app.infoBox("MsgDone", "Saved!", parent=None)
         elif not example:
-            smellDocumentation = [(smellName, description, rationale, causes, '', affected, abstractions, considerations)]
-            c.executemany('INSERT INTO smell VALUES (?,?,?,?,?,?,?,?)', smellDocumentation)
+            smellDocumentation = [(smellName, description, rationale, causes, exampleDesc, '', example, affected, abstractions, considerations)]
+            c.executemany('INSERT INTO smell VALUES (?,?,?,?,?,?,?,?,?,?)', smellDocumentation)
             conn.commit()
             app.clearAllEntries(callFunction=False)
             app.clearAllTextAreas(callFunction=False)
@@ -63,18 +64,14 @@ def insertSmell():
             if example.endswith('png'):
                 png = open(example,'rb')
                 ablob = png.read()
-                smellDocumentation = [(description, rationale, causes, sqlite3.Binary(ablob), affected, abstractions, considerations, smellName)]
-                c.executemany('UPDATE smell SET Description=?, rationale=?, causes=?, example=?, affected=?, abstractions=?, considerations = ? WHERE Smell = ?',smellDocumentation)
+                smellDocumentation = [(description, rationale, causes, exampleDesc, sqlite3.Binary(ablob), example, affected, abstractions, considerations, smellName)]
+                c.executemany('UPDATE smell SET Description=?, rationale=?, causes=?, exampleDesc = ?, figure = ?, pngpath=?, affected=?, abstractions=?, considerations = ? WHERE Smell = ?',smellDocumentation)
                 conn.commit()
-                app.clearAllEntries(callFunction=False)
-                app.clearAllTextAreas(callFunction=False)
                 app.infoBox("MsgUpdate", "Updated!", parent=None)
             elif not example:
-                smellDocumentation = [(description, rationale, causes, '', affected, abstractions, considerations, smellName)]
-                c.executemany('UPDATE smell SET Description=?, rationale=?, causes=?, example=?, affected=?, abstractions=?, considerations = ? WHERE Smell = ?', smellDocumentation)
+                smellDocumentation = [(description, rationale, causes, exampleDesc, '', example, affected, abstractions, considerations, smellName)]
+                c.executemany('UPDATE smell SET Description=?, rationale=?, causes=?, exampleDesc = ?, figure = ?, pngpath=?, affected=?, abstractions=?, considerations = ? WHERE Smell = ?', smellDocumentation)
                 conn.commit()
-                app.clearAllEntries(callFunction=False)
-                app.clearAllTextAreas(callFunction=False)
                 app.infoBox("MsgUpdate", "Updated!", parent=None)
             else:
                 app.infoBox("MsgInsert", "The file is not a PNG file!. Data was not updated!.", parent=None)   
@@ -89,19 +86,27 @@ def searchSmell(op):
         if row == None:
              app.infoBox("MsgEmpty", "The smell does not exist!", parent=None)
         else:
+            app.clearAllEntries(callFunction=False)
+            app.clearAllTextAreas(callFunction=False)
             smell = row[0]
             description = row[1]
             rationale = row[2]
             causes = row[3]
-            affected = row[5]
-            abstractions = row[6]
-            considerations = row[7]
+            exampleDesc = row[4]
+            pngpath = row[6]
+            affected = row[7]
+            abstractions = row[8]
+            considerations = row[9]
+            app.setEntry("Smell",smell)
             app.setTextArea("Description",description)
             app.setTextArea("implRationale",rationale) 
             app.setTextArea("implPotential",causes)
+            app.setTextArea("exDesc",exampleDesc)
+            app.setEntry("f1",pngpath)
             app.setTextArea("implQualityAttr",affected)
             app.setTextArea("implAbstrAffec",abstractions)
             app.setTextArea("impllblPractialCons",considerations)
+        c.close()
     else:
         c = conn.cursor()
         c.execute("SELECT * FROM smell WHERE smell = ?",[op])
@@ -123,7 +128,6 @@ def deleteSmell():
     app.clearAllEntries(callFunction=False)
     app.clearAllTextAreas(callFunction=False)
     
-
 def getTableHeader():
     c = conn.cursor()
     c.execute("SELECT smell, description FROM smell")
@@ -167,7 +171,6 @@ def press(button):
             deleteSmell()
 
 def processRow(pos):
-  
     list1 = app.getTableRow("SmellTable", pos)
     var = list1[0]
     c = conn.cursor()
@@ -177,10 +180,11 @@ def processRow(pos):
     description = row[1]
     rationale = row[2]
     causes = row[3]
-    png = row[4]
-    affected = row[5]
-    abstractions = row[6]
-    considerations = row[7]
+    exDesc = row[4]
+    png = row[5]
+    affected = row[7]
+    abstractions = row[8]
+    considerations = row[9]
     
     #Description
     document = Document()
@@ -223,13 +227,17 @@ def processRow(pos):
     paragraph_format = p.paragraph_format
     paragraph_format.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
     
-    #Examples
+    
+    #Example and Figure
     p = document.add_paragraph()
     run = p.add_run('Examples: ')
     run.bold = True
     run.underline = True
     font = run.font
     font.size = Pt(12)
+    p = document.add_paragraph(exDesc)
+    paragraph_format = p.paragraph_format
+    paragraph_format.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
     if png != '':
         if not os.path.exists(smell):
             os.makedirs(smell)
@@ -285,11 +293,10 @@ def onChangeTF(name):
         app.deleteAllTableRows("SmellTable")
         app.addTableRows("SmellTable", rows)
 
-
 # handle menuPress
 def menu(name):
     if name == "About":
-        print("Hello")
+        app.infoBox("Information", "Smelly SAs Catalog 2018.\nCreated by Daniel San Martin\ndsanmartins@gmail.com", parent=None)
 
 ##########################################################################################################################################################################
 # Widgets implementation
@@ -300,7 +307,7 @@ createTables()
 
 
 # create a GUI variable called app
-app = gui("Architectural Smells for SAs Catalog", "1024x768")
+app = gui("Architectural Smells for SAs Catalog", "1350x768")
 app.setBg("black")
 app.setFont(18)
 
@@ -313,128 +320,142 @@ app.setLabelFg("lblTitle", "white")
 app.getLabelWidget("lblTitle").config(font="Verdana 18 italic bold" )
 
 app.startTabbedFrame("TabbedFrame")
-app.startTab("Add Architectural Smell")
+app.startTab("Architectural Smell")
 app.setBg("black")
 app.setFg("white")
-
 app.setTabbedFrameChangeFunction("TabbedFrame", onChangeTF) 
 
 
-
 #1-0
-app.addLabelEntry("Smell",1,0)
-app.setLabelBg("Smell", "black")
-app.setLabelFg("Smell", "white")
-app.getLabelWidget("Smell").config(font="Verdana 12 italic bold" )
-app.setEntryMaxLength("Smell", 40)
-#1-3
-#app.addLabel("Name","Nothing", 1,1)
-app.addMessage("Name", "Information \n-----------------\n\nSmell & Description: A concise, intuitive name based on our naming scheme (comprises two words: first word is an adjective, and second word is the primarily violated design principle). The name is followed by a concise description of the architectural smell (along with its possible forms).\n--------------------------------------------------------------------------------\nRationale: Reason/justification for the architectural smell in the context of adaptive systems.\n--------------------------------------------------------------------------------\nPotential Causes: List of typical reasons for the occurrence of the smell (a nonexhaustive list based on our experience).\n--------------------------------------------------------------------------------\nExamples: One or more examples highlighting the smell. If a smell has multiple forms, each form may be illustrated using a specific example (Figures in PNG).\n--------------------------------------------------------------------------------\nImpacted Quality Attributes: The quality attributes that are negatively impacted because of this smell. It may include understandability, changeability, extensibility, reusability, testability, reliability among others.\n--------------------------------------------------------------------------------\nAffected Architectural Abstractions: Adaptive System Abstractions affected by the architectural smell.\n--------------------------------------------------------------------------------\nPractical Considerations: Sometimes, in a real-world context, a particular design decision that introduces a smell may be purposely made either due to constraints (such as language or platform limitations) or to address a larger problem in the overall design and implementation.",1,1,1,8)
-app.setMessageBg("Name", "black")
-app.setMessageFg("Name", "white")
-app.getMessageWidget("Name").config(width="400", font="Verdana 10 italic bold" )
-#2-0
-
-app.startFrame("frm1", row=2, column=0)
+app.startFrame("frm0", row=1, column=0)
 app.setBg("black")
-app.setSticky("NEW")
-app.setStretch("COLUMN")
+app.setSticky("w")
+app.setStretch("none")
 
-app.addLabel("lblSmellDesc","Description")
+app.addLabel("lblSmell", "Smell Name")
+app.setLabelBg("lblSmell", "black")
+app.setLabelFg("lblSmell", "white")
+app.getLabelWidget("lblSmell").config(font="Verdana 12 italic bold")
+app.addEntry('Smell')
+app.setEntryMaxLength("Smell", 40)
+app.getEntryWidget("Smell").config(width=40)
+app.stopFrame()
+
+#1-1
+app.startFrame("frm1", row=1, column=1)
+app.setBg("black")
+app.setSticky("w")
+app.setStretch("none")
+
+app.addLabel("lblSmellDesc","A Brief Description of the Smell")
 app.setLabelBg("lblSmellDesc", "black")
 app.setLabelFg("lblSmellDesc", "white")
 app.getLabelWidget("lblSmellDesc").config(font="Verdana 12 italic bold" )
-app.addScrolledTextArea('Description', 2,0)
-app.getTextAreaWidget("Description").config(height="3")
+app.addScrolledTextArea('Description')
+app.getTextAreaWidget("Description").config(height=4, width= 70)
 app.stopFrame()
 
-#3-0
-app.startFrame("frm2", row=3, column=0)
+#2-0
+app.startFrame("frm2", row=2, column=0)
 app.setBg("black")
-app.setSticky("NEW")
-app.setStretch("COLUMN")
+app.setSticky("w")
+app.setStretch("none")
 
 app.addLabel("lblRationale","Rationale")
 app.setLabelBg("lblRationale", "black")
 app.setLabelFg("lblRationale", "white")
 app.getLabelWidget("lblRationale").config(font="Verdana 12 italic bold" )
-app.addScrolledTextArea('implRationale', 2,0)
-app.getTextAreaWidget("implRationale").config(height="3")
+app.addScrolledTextArea('implRationale')
+app.getTextAreaWidget("implRationale").config(height="4" , width= 70)
 app.stopFrame()
 
-#4-0
-app.startFrame("frm3", row=4, column=0)
+#2-1
+app.startFrame("frm3", row=2, column=1)
 app.setBg("black")
-app.setSticky("NEW")
-app.setStretch("COLUMN")
+app.setSticky("w")
+app.setStretch("none")
 
 app.addLabel("lblPotential","Potential Causes")
 app.setLabelBg("lblPotential", "black")
 app.setLabelFg("lblPotential", "white")
 app.getLabelWidget("lblPotential").config(font="Verdana 12 italic bold" )
-app.addScrolledTextArea('implPotential', 2,0)
-app.getTextAreaWidget("implPotential").config(height="3")
+app.addScrolledTextArea('implPotential')
+app.getTextAreaWidget("implPotential").config(height="4" , width= 70)
 app.stopFrame()
-#5-0
-app.startFrame("frm4", row=5, column=0)
-app.setBg("black")
-app.setSticky("NEW")
-app.setStretch("COLUMN")
 
-app.addLabel("lblExamples","Examples (PNG file)")
+#3-0
+app.startFrame("frm4", row=3, column=0)
+app.setBg("black")
+app.setSticky("w")
+app.setStretch("none")
+
+app.addLabel("lblExampleDescription","A Description of an Example")
+app.setLabelBg("lblExampleDescription", "black")
+app.setLabelFg("lblExampleDescription", "white")
+app.getLabelWidget("lblExampleDescription").config(font="Verdana 12 italic bold" )
+app.addScrolledTextArea("exDesc")
+app.getTextAreaWidget("exDesc").config(height=4, width =70)
+app.stopFrame()
+
+#3-1
+app.startFrame("frm5", row=3, column=1)
+app.setBg("black")
+app.setSticky("w")
+app.setStretch("none")
+
+app.addLabel("lblExamples","A Figure of the Example (PNG file)")
 app.setLabelBg("lblExamples", "black")
 app.setLabelFg("lblExamples", "white")
 app.getLabelWidget("lblExamples").config(font="Verdana 12 italic bold" )
-app.addFileEntry("f1",4,0)
+app.addFileEntry("f1")
+app.getEntryWidget("f1").config(width =44)
 app.stopFrame()
 
-
-#6-0
-app.startFrame("frm5", row=6, column=0)
+#4-0
+app.startFrame("frm6", row=4, column=0)
 app.setBg("black")
-app.setSticky("NEW")
-app.setStretch("COLUMN")
+app.setSticky("w")
+app.setStretch("none")
 
 app.addLabel("lblQualityAttr","Impacted Quality Attributes")
 app.setLabelBg("lblQualityAttr", "black")
 app.setLabelFg("lblQualityAttr", "white")
 app.getLabelWidget("lblQualityAttr").config(font="Verdana 12 italic bold" )
-app.addScrolledTextArea('implQualityAttr', 2,0)
-app.getTextAreaWidget("implQualityAttr").config(height="2")
+app.addScrolledTextArea('implQualityAttr')
+app.getTextAreaWidget("implQualityAttr").config(height=4, width= 70)
 app.stopFrame()
 
-#7-0
-app.startFrame("frm6", row=7, column=0)
+#4-1
+app.startFrame("frm7", row=4, column=1)
 app.setBg("black")
-app.setSticky("NEW")
-app.setStretch("COLUMN")
+#app.setSticky("e")
+app.setStretch("none")
 
 app.addLabel("lblAbstrAffec","Affected Architectural Abstractions")
 app.setLabelBg("lblAbstrAffec", "black")
 app.setLabelFg("lblAbstrAffec", "white")
 app.getLabelWidget("lblAbstrAffec").config(font="Verdana 12 italic bold" )
-app.addScrolledTextArea('implAbstrAffec', 2,0)
-app.getTextAreaWidget("implAbstrAffec").config(height="2")
+app.addScrolledTextArea('implAbstrAffec')
+app.getTextAreaWidget("implAbstrAffec").config(height=4, width= 70)
 app.stopFrame()
 
-#8-0
-app.startFrame("frm7", row=8, column=0)
+#5-0
+app.startFrame("frm8", row=5, column=0)
 app.setBg("black")
-app.setSticky("NEW")
-app.setStretch("COLUMN")
+#app.setSticky("e")
+app.setStretch("none")
 
 app.addLabel("lblPractialCons","Practical Considerations")
 app.setLabelBg("lblPractialCons", "black")
 app.setLabelFg("lblPractialCons", "white")
 app.getLabelWidget("lblPractialCons").config(font="Verdana 12 italic bold" )
-app.addScrolledTextArea('impllblPractialCons', 2,0)
-app.getTextAreaWidget("impllblPractialCons").config(height="3")
+app.addScrolledTextArea('impllblPractialCons')
+app.getTextAreaWidget("impllblPractialCons").config(height=4, width= 70)
 app.stopFrame()
 
 #add menu
 fileMenus = ["About"]
-app.addMenuList("About", fileMenus, menu)
-
+app.addMenuList("Menu", fileMenus, menu)
 
 # link the buttons to the function called press
 #app.startFrame("frm8", row=8, column=0)
@@ -445,15 +466,22 @@ app.startTab("List of Architectural Smells")
 app.setBg("black")
 app.setFg("white")
 header = getTableHeader()
-app.addTable("SmellTable", header, action=processRow, actionHeading= "PDF")
+app.addTable("SmellTable", header, action=processRow, actionHeading= "Word")
 rows = getAllSmell()
 app.addTableRows("SmellTable", rows)
-app.getTableWidget('SmellTable').config(width=40, height=50)
+app.stopTab()
+
+app.startTab("Information")
+app.setBg("black")
+app.setFg("white")
+app.addMessage("Name", "Smell & Description: A concise, intuitive name based on our naming scheme (comprises two words: first word is an adjective, and second word is the primarily violated design principle). The name is followed by a concise description of the architectural smell (along with its possible forms).\n--------------------------------------------------------------------------------\nRationale: Reason/justification for the architectural smell in the context of adaptive systems.\n--------------------------------------------------------------------------------\nPotential Causes: List of typical reasons for the occurrence of the smell (a nonexhaustive list based on our experience).\n--------------------------------------------------------------------------------\nExamples: One or more examples highlighting the smell. If a smell has multiple forms, each form may be illustrated using a specific example (Figures in PNG).\n--------------------------------------------------------------------------------\nImpacted Quality Attributes: The quality attributes that are negatively impacted because of this smell. It may include understandability, changeability, extensibility, reusability, testability, reliability among others.\n--------------------------------------------------------------------------------\nAffected Architectural Abstractions: Adaptive System Abstractions affected by the architectural smell.\n--------------------------------------------------------------------------------\nPractical Considerations: Sometimes, in a real-world context, a particular design decision that introduces a smell may be purposely made either due to constraints (such as language or platform limitations) or to address a larger problem in the overall design and implementation.",1,1,1,8)
+app.setMessageBg("Name", "black")
+app.setMessageFg("Name", "white")
+app.getMessageWidget("Name").config(width="1200", font="Verdana 12 italic bold" )
 app.stopTab()
 
 app.stopTabbedFrame()
 app.setTabbedFrameActiveFg("TabbedFrame", "white")
-#app.getTabbedFrameWidget("TabbedFrame").config(font="Verdana")
 
 
 # start the GUI
